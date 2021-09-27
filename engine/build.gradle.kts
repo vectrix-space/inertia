0f8422e6f77a87056678f629bfc95525fb5fe455
connorhartley <vectrixu@gmail.com>
2021-09-27 13:50:09 +1300

    bump checker-qual to 3.18.0, guava to 31.0, fastutil to 8.5.6 and flare to 1.0.0

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 dependencies {
-  api("com.google.guava:guava:30.1.1-jre")
-  api("it.unimi.dsi:fastutil:8.5.4")
+  compileOnlyApi("org.checkerframework:checker-qual:3.18.0")
+  api("com.google.guava:guava:31.0-jre")
   api("net.kyori:coffee:1.0.0-SNAPSHOT")
-  api("org.checkerframework:checker-qual:3.16.0")
-  api("space.vectrix.flare:flare:0.3.1-SNAPSHOT")
-  api("space.vectrix.flare:flare-fastutil:0.3.1-SNAPSHOT")
+  api("it.unimi.dsi:fastutil:8.5.6")
+  api("space.vectrix.flare:flare:1.0.0")
+  api("space.vectrix.flare:flare-fastutil:1.0.0")
 }
 
 applyJarMetadata("space.vectrix.inertia")
